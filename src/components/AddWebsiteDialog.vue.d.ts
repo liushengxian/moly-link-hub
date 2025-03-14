12e9c624ea1ca6a4462e942d2f57471433f3adc0
Shawn <shawn@ShawndeMacBook-Pro.local>
2025-03-14 23:56:25 +0800

    Revert "second commit"
    This reverts commit 99229f0e3e0fed5020a15b7bd4cdf4c37c5f5b57.

--- src/components/AddWebsiteDialog.vue.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: import("vue").DefineComponent<{}, {}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {
-    close: (...args: any[]) => void;
-    add: (...args: any[]) => void;
-}, string, import("vue").PublicProps, Readonly<{}> & Readonly<{
-    onClose?: ((...args: any[]) => any) | undefined;
-    onAdd?: ((...args: any[]) => any) | undefined;
-}>, {}, {}, {}, {}, string, import("vue").ComponentProvideOptions, true, {}, any>;
-export default _default;
