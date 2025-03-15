e587e1752fb2dcf6d8b6249bff0c23e46a628032
liushengxian <liushengxian1230@163.com>
2025-03-15 04:58:23 +0000

    Deploying to gh-pages from @ liushengxian/moly-link-hub@271d39492c80d7059b6ed1a9f8fa4f0c3a6401c0 🚀

---
+++ assets/Settings-Ct4SG0TZ.js
@@ -0,0 +1,2 @@
+import{_ as t,c as n,a as s,o}from"./index-DHkSAvi6.js";const a={},i={class:"settings-container"};function c(r,e){return o(),n("div",i,e[0]||(e[0]=[s("h1",null,"Settings",-1),s("p",null,"This is the settings page. More options will be available soon.",-1)]))}const _=t(a,[["render",c],["__scopeId","data-v-ad9bf1d8"]]);export{_ as default};
+//# sourceMappingURL=Settings-Ct4SG0TZ.js.map
